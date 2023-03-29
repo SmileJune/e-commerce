8c4969346a6a4e88ba3cea0357d840657e8faf3d
SmileJun <godhkekf24@inha.edu>
2023-03-29 20:48:08 +0900

    config-service 구현

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.userservice.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,18 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/user-service")
 public class UserController {
 
+    Environment env;
+
+    UserController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/health_check")
-    public String healthCheck(HttpServletRequest request) {
-        return String.format("It's Working in User Service on Port %s", request.getServerPort());
+    public String healthCheck() {
+        return String.format("It's Working in User Service"
+            + ", port(local.server.port)=" + env.getProperty("local.server.port")
+            + ", port(server.port)=" + env.getProperty("server.port")
+            + ", token secret=" + env.getProperty("token.secret")
+            + ", token expiration time=" + env.getProperty("token.expiration_time"));
     }
 }
